eb8e6ef0ae5da01fc7f0fdd8749d2f6adf8fa621
sherwino <sherwino@outlook.com>
2019-07-10 10:01:40 -0400

    Removing VSCode for now

--- src/VSCode/VSCode.js
+++ src/VSCode/VSCode.js
@@ -23,32 +23,34 @@ class VSCode extends React.Component {
   
   render() {
     return (
-      <Rnd
-        id="window"
-        bounds="window"
-        size={{ width: this.state.width, height: this.state.height }}
-        position={{ x: this.state.x, y: this.state.y }}
-        onDragStop={(e, d) => {
-          this.setState({ x: d.x, y: d.y });
-        }}
-        onResize={(e, direction, ref, delta, position) => {
-          this.setState({
-            width: ref.style.width,
-            height: ref.style.height,
-            ...position
-          });
-        }}
-      >
-      <div style={style}>Visual Studio Code</div>
-      <Iframe url="http://ec2-34-238-162-153.compute-1.amazonaws.com"
-        width={this.state.width}
-        height={this.state.height}
-        position={{ x: this.state.x, y: this.state.y }}
-        id="vscode-iframe"
-        className="iframe"
-        display="initial"
-        allowFullScreen/>
-      </Rnd>
+      <>
+      </>
+      // <Rnd
+      //   id="window"
+      //   bounds="window"
+      //   size={{ width: this.state.width, height: this.state.height }}
+      //   position={{ x: this.state.x, y: this.state.y }}
+      //   onDragStop={(e, d) => {
+      //     this.setState({ x: d.x, y: d.y });
+      //   }}
+      //   onResize={(e, direction, ref, delta, position) => {
+      //     this.setState({
+      //       width: ref.style.width,
+      //       height: ref.style.height,
+      //       ...position
+      //     });
+      //   }}
+      // >
+      // <div style={style}>Visual Studio Code</div>
+      // <Iframe url="http://ec2-34-238-162-153.compute-1.amazonaws.com"
+      //   width={this.state.width}
+      //   height={this.state.height}
+      //   position={{ x: this.state.x, y: this.state.y }}
+      //   id="vscode-iframe"
+      //   className="iframe"
+      //   display="initial"
+      //   allowFullScreen/>
+      // </Rnd>
     );
   }
 }
