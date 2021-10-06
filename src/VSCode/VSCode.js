c4350056ce135a2187391a4821eaf398e37df45e
Sherwino <s.garcia@applicaster.com>
2021-10-06 01:08:55 -0400

    fix: use codesandbox

--- src/VSCode/VSCode.js
+++ src/VSCode/VSCode.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Rnd } from "react-rnd";
-import Iframe from "react-iframe";
 
 const style = {
-  background: "#000",
-  paddingLeft: "15px",
+  background: "url(https://i.gyazo.com/9c73502f78d3e1f810d79cab6ec2464a.png)",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  margin: 0,
+  borderLeft: "2px",
+  borderRight: "2px",
+  borderColor: "#ddd",
+  borderStyle: "solid",
   width: "100%",
   color: "#fff",
-  boxSizing: "border-box"
+  height: 25,
 };
 
 class VSCode extends React.Component {
@@ -17,47 +22,50 @@ class VSCode extends React.Component {
       width: "830",
       height: "690",
       x: "100",
-      y: "40"
+      y: "40",
     };
   }
 
-
-  
   render() {
-  const position = JSON.stringify({ x: this.state.x, y: this.state.y });
-  const width = this.state.width.toString();
-  const height = this.state.height.toString();
+    const position = { x: this.state.x, y: this.state.y };
+    const width = this.state.width;
+    const height = this.state.height;
 
     return (
       <>
-    <Rnd
-      id="window"
-      bounds="window"
-      size={{ width, height }}
-      position={position}
-      onDragStop={(e, d) => {
-        this.setState({ x: d.x, y: d.y });
-      }}
-      onResize={(e, direction, ref, delta, position) => {
-        this.setState({
-          width: ref.style.width,
-          height: ref.style.height,
-          ...position
-        });
-      }}
-    >
-    <div style={style}>Visual Studio Code</div>
-    {/* https://winodoescors.sherwino.repl.co/ */}
-    <Iframe url="https://vscode-web-test-playground.azurewebsites.net/"
-      width={width}
-      height={height}
-      position={position}
-      id="vscode-iframe"
-      className="iframe"
-      display="initial"
-      allowFullScreen
-      />
-    </Rnd>
+        <Rnd
+          id="window"
+          bounds="window"
+          size={{ width, height }}
+          position={position}
+          onDrag={(e, d) => {
+            this.setState({ x: d.x, y: d.y });
+          }}
+          // onDragStop={(e, d) => {
+          //   this.setState({ x: d.x, y: d.y });
+          // }}
+          onResize={(e, direction, ref, delta, position) => {
+            this.setState({
+              width: ref.style.width,
+              height: ref.style.height,
+              ...position,
+            });
+          }}
+        >
+          <div style={style} />
+          {/* https://winodoescors.sherwino.repl.co/ */}
+          <iframe
+            src="https://codesandbox.io/s/infallible-bell-cgxgz"
+            title="code"
+            width={width}
+            height={height}
+            position={position}
+            id="vscode-iframe"
+            className="iframe"
+            display="initial"
+            allowFullScreen
+          />
+        </Rnd>
       </>
     );
   }
