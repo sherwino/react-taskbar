0bf4bbaf13d3300236803494ac613cce9985093f
Sherwino <s.garcia@applicaster.com>
2021-10-06 00:11:33 -0400

    fix: use git dev

--- src/VSCode/VSCode.js
+++ src/VSCode/VSCode.js
@@ -14,43 +14,51 @@ class VSCode extends React.Component {
   constructor() {
     super();
     this.state = {
-      width: 830,
-      height: 690,
-      x: 100,
-      y: 40
+      width: "830",
+      height: "690",
+      x: "100",
+      y: "40"
     };
   }
+
+
   
   render() {
+  const position = JSON.stringify({ x: this.state.x, y: this.state.y });
+  const width = this.state.width.toString();
+  const height = this.state.height.toString();
+
     return (
       <>
+    <Rnd
+      id="window"
+      bounds="window"
+      size={{ width, height }}
+      position={position}
+      onDragStop={(e, d) => {
+        this.setState({ x: d.x, y: d.y });
+      }}
+      onResize={(e, direction, ref, delta, position) => {
+        this.setState({
+          width: ref.style.width,
+          height: ref.style.height,
+          ...position
+        });
+      }}
+    >
+    <div style={style}>Visual Studio Code</div>
+    {/* https://winodoescors.sherwino.repl.co/ */}
+    <Iframe url="https://github.dev/sherwino/react-taskbar"
+      width={width}
+      height={height}
+      position={position}
+      id="vscode-iframe"
+      className="iframe"
+      display="initial"
+      allowFullScreen
+      />
+    </Rnd>
       </>
-      // <Rnd
-      //   id="window"
-      //   bounds="window"
-      //   size={{ width: this.state.width, height: this.state.height }}
-      //   position={{ x: this.state.x, y: this.state.y }}
-      //   onDragStop={(e, d) => {
-      //     this.setState({ x: d.x, y: d.y });
-      //   }}
-      //   onResize={(e, direction, ref, delta, position) => {
-      //     this.setState({
-      //       width: ref.style.width,
-      //       height: ref.style.height,
-      //       ...position
-      //     });
-      //   }}
-      // >
-      // <div style={style}>Visual Studio Code</div>
-      // <Iframe url="http://ec2-34-238-162-153.compute-1.amazonaws.com"
-      //   width={this.state.width}
-      //   height={this.state.height}
-      //   position={{ x: this.state.x, y: this.state.y }}
-      //   id="vscode-iframe"
-      //   className="iframe"
-      //   display="initial"
-      //   allowFullScreen/>
-      // </Rnd>
     );
   }
 }
