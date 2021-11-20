00083b02e850526093704519c7534f52aec60444
Sherwino <s.garcia@applicaster.com>
2021-11-20 17:35:09 -0500

    Updates

---
+++ precache-manifest.655c35203e1b7217b67eaa3dd821cb2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "309ae354dd0e1a4f9ac4",
+    "url": "/static/css/main.f8ef6df9.chunk.css"
+  },
+  {
+    "revision": "309ae354dd0e1a4f9ac4",
+    "url": "/static/js/main.309ae354.chunk.js"
+  },
+  {
+    "revision": "c44823bb082c7765cb26",
+    "url": "/static/css/1.91892d2a.chunk.css"
+  },
+  {
+    "revision": "c44823bb082c7765cb26",
+    "url": "/static/js/1.c44823bb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "45c49d7f4e7f09238f485d5a9bc82a19",
+    "url": "/static/media/explorer.45c49d7f.png"
+  },
+  {
+    "revision": "7689d648030368a4c561f79633799f1f",
+    "url": "/index.html"
+  }
+];
