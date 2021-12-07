714b14f8dee6108c4b2d91aa0ef4598eed2c62ac
Sherwino <s.garcia@applicaster.com>
2021-12-07 03:05:28 -0500

    fix: add back the clock

--- src/Taskbar/Taskbar.tsx
+++ src/Taskbar/Taskbar.tsx
@@ -9,13 +9,13 @@ const Taskbar = (props: any) => {
   const [dataTime, setTime] = useState({ time: new Date() });
   const { time } = dataTime;
   // const context = React.useContext(WindowContext);
-  // const currentTime = () => setTime({ time: new Date() });
+  const currentTime = () => setTime({ time: new Date() });
 
  
   useEffect(() => {
-    // const int = setInterval(() => currentTime(), 1000);
+    const int = setInterval(() => currentTime(), 1000);
 
-    // return () => clearInterval(int);
+    return () => clearInterval(int);
   }, []);
 
   return (
